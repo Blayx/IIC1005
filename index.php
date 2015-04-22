5b55120a55f33fc4f676d880ab91b8309577584b
Blayx <jamunoz14@uc.cl>
2015-04-22 19:46:02 -0400

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+  <head>
+    <title>Chat :D</title>
+  </head>
+  <body>
+    <?php
+    echo "hey there";
+    ?>
+    <div id='main'>
+	<div id='chat'></div>
+	  <form onSubmit='alert(datos); return false;'>
+	    <input type='text' id='escritura'>
+	    <input type='submit'>
+	  </form>
+    </div>
+  </body>
+</html>
